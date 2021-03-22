59e76ebd8274cd6cc42e281758cb7ac14dad3f41
Kazuki Tsujimura <63531246+kazuki0122@users.noreply.github.com>
2021-03-22 12:27:55 +0900

    ログイン・新規登録実装

---
+++ src/components/Signin.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-app>
+    <v-card width="400px" class="mx-auto mt-5">
+      <v-card-title>
+        <h1 class="dispay-1">Sign in</h1>
+      </v-card-title>
+      <v-card-text>
+        <v-form>
+          <v-text-field 
+          prepend-icon="mdi-email" 
+          label="email" 
+          v-model="email"
+          />
+          <v-text-field 
+          v-bind:type="showPassword ? 'text' : 'password'"
+          @click:append="showPassword = !showPassword"
+          prepend-icon="mdi-lock"
+          v-bind:append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+          label="パスワード" 
+          v-model="password"
+          />
+          <v-card-actions>
+            <v-btn class="info" @click="submit">Sign in</v-btn>
+          </v-card-actions>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-app>
+</template>
+
+<script>
+import firebase from 'firebase'
+export default {
+  name: 'app',
+  data:() => ({
+    showPassword: false,
+    email:'',
+    password:''
+  }),
+  methods:{
+    submit(){
+       firebase.auth().signInWithEmailAndPassword(this.email, this.password)
+       .then(() => {
+          alert('Success!')
+          this.$router.push('/')
+        },
+        err => {
+          alert(err.message)
+        }
+      )
+    }
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import router from './router';
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const config = {
+  apiKey: "AIzaSyBecLHNpPYKBLZhLxE6hhsF-tr4Eoart_A",
+  authDomain: "todo-app-d5250.firebaseapp.com",
+  projectId: "todo-app-d5250",
+  storageBucket: "todo-app-d5250.appspot.com",
+  messagingSenderId: "315696190139",
+  appId: "1:315696190139:web:9dd5a1f3414ebb49f5cf9e",
+  measurementId: "G-8TZM9MDZYY"
+};
+
+firebase.initializeApp(config);
+
 new Vue({
+  router,
   vuetify,
   render: h => h(App)
 }).$mount('#app')
